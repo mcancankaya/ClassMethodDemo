4952b97c3f51fea414a909fe4027126047bcb612
Can <Can@DESKTOP-HRQ3BVA>
2022-01-29 02:17:31 +0300

    Add project files.

---
+++ ClassMethodDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi : "+musteri.Adi);
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi : "+musteri.Adi);
+        }
+        public void Update(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Güncellendi : "+musteri.Adi);
+        }
+        public void GetAllMusteri(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri Adı : "+musteri.Adi+"\nMüşteri Soyadı : "+musteri.Soyadi+"\nMüşteri Telefon : "+musteri.Telefon);
+                Console.WriteLine("-----  -----  -----  -----  -----");
+            }
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id =1;
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.Telefon = "01234567890";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Mert";
+            musteri2.Soyadi = "YMG";
+            musteri2.Telefon = "01234567890";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Adi = "Can";
+            musteri3.Soyadi = "YMG";
+            musteri3.Telefon = "01234567890";
+
+            Musteri[] musteriler = {musteri1,musteri2,musteri3 };
+
+            MusteriManager manager = new MusteriManager();
+            manager.Add(musteri1);
+            manager.Delete(musteri2);
+            manager.Update(musteri3);
+            Console.WriteLine("----- Müşteri Listesi -----");
+            manager.GetAllMusteri(musteriler);
+        }
+    }
+}
